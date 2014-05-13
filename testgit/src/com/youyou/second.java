976e6d76fd8173eda9cd328c3779e0dd7e90f576
youyoujia <584115300@qq.com>
2014-05-13 15:51:29 +0800

    this is my first try

---
+++ testgit/src/com/youyou/second.java
@@ -0,0 +1,10 @@
+package com.youyou;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first submit");
+	}
+
+}
